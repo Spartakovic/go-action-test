678e5dabb1009a6704478ae65f0e94a49a615881
Dias Saparov <dias@digger.dev>
2023-03-09 13:03:42 +0000

    test

--- main.go
+++ main.go
@@ -1,6 +1,37 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"github.com/hashicorp/terraform-exec/tfexec"
+	"log"
+	"os/exec"
+)
+
 // write code to print something to the console
 func main() {
-	println("Hello, world!")
+	workingDir := "."
+
+	tfPath, err := exec.LookPath("terraform")
+
+	if err != nil {
+		log.Fatalf("error running LookPath: %s", err)
+	}
+
+	tf, err := tfexec.NewTerraform(workingDir, tfPath)
+	if err != nil {
+		log.Fatalf("error running NewTerraform: %s", err)
+	}
+
+	err = tf.Init(context.Background(), tfexec.Upgrade(true))
+	if err != nil {
+		log.Fatalf("error running Init: %s", err)
+	}
+
+	state, err := tf.Show(context.Background())
+	if err != nil {
+		log.Fatalf("error running Show: %s", err)
+	}
+
+	fmt.Println(state.FormatVersion)
 }
